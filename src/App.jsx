459dc182b8676604689f945097d8979d7650da58
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-06-11 22:06:06 +0530

    Todo list project created

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TodoList from "./TodoList";"./TodoList";
+function App() {
+  return (
+    <div>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
+
---
+++ src/TodoList.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function TodoList(){
+    return (
+        <div>
+            <h2>To Do List1</h2>
+        </div>
+    )
+}
+
+export default TodoList;
