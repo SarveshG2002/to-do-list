d955a2c24b30b2ae4ec3d671f232cb3ece0d6e31
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-06-13 21:00:37 +0530

    navbar created

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,14 +5,18 @@ import './Navbar.css'; // Import custom CSS for the navbar
 function Navbar() {
     return (
         <nav className="">
-            <div className='username'>
-                Sarvesh@gmailcom
+            <div className='navele'>
+                <div className='username'>
+                    Sarvesh@gmailcom
+                </div>
+                &emsp;
+                <div className='searchBar'>
+                    <input type='search' placeholder ='Search . . .' ></input>
+                </div>
             </div>
-            &emsp;
-            <div className='searchBar'>
-                <input type='search' placeholder ='Search . . .' ></input>
+            <div className='navele'>
+                <button className='logoutButton' onClick={() => { localStorage.removeItem('username'); window.location.reload(); }}>Logout</button>
             </div>
-            <button className='logoutButton' onClick={() => { localStorage.removeItem('username'); window.location.reload(); }}>Logout</button>
         </nav>
     );
 }
