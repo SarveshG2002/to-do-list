170752fe988066f3dbcd31334b8a321364fe6c34
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-06-21 21:39:21 +0530

    Today Task And Daily Task Seperated

--- src/components/Inbox.jsx
+++ src/components/Inbox.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
-import React, { useState,useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BASE_URL } from './Host.jsx';
 function Inbox() {
-    const [text,setText] = useState("");
+    const [text, setText] = useState("");
     const [tasks, setTasks] = useState([]);
     const [error, setError] = useState("");
 
@@ -28,25 +28,25 @@ function Inbox() {
         }
     };
 
-    const handleSubmit = async (e)=>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        try{
+        try {
             console.log(text)
             const username = localStorage.getItem('username');
             // if (!username) {
             //     throw new Error('Username not found in localStorage');
             // }
-            const response = await axios.post(`${BASE_URL}/api/addtodayTask`,{
+            const response = await axios.post(`${BASE_URL}/api/addtodayTask`, {
                 username: username,
-                task:text
+                task: text
             })
             console.log(response.data)
-            if(response.data.success){
+            if (response.data.success) {
                 setText("");
                 fetchTasks();
-                
+
             }
-        }catch (error) {
+        } catch (error) {
             console.error("Error during login:", error);
             setError('An error occurred. Please try again.');
         }
@@ -57,26 +57,42 @@ function Inbox() {
             <form className='new-add' onSubmit={handleSubmit}>
                 <div class="form-group required">
                     {/* <input type="text" className='form-control'/> */}
-                    <textarea value={text} onChange={(e)=>setText(e.target.value)}></textarea>
+                    <textarea value={text} onChange={(e) => setText(e.target.value)}></textarea>
                 </div>
                 <button className='btn btn-primary mt-3'>Add</button>
             </form>
 
             <div className='to-do'>
-                <div className='Heading'>
-                    Today Task
-                </div>
-                {tasks.map(task => (
-                    <div className='task' key={task.id}>
-                        <textarea defaultValue={task.task}></textarea>
-                        <div>
-                            <input type='checkbox' id={`check${task.id}`} defaultChecked={task.status === 'complete'} /> &nbsp;
-                            <label htmlFor={`check${task.id}`}>Done</label>
-                        </div>
+                <div className='taskGroup'>
+                    <div className='Heading'>
+                        Today Specific Task
                     </div>
-                ))}
+                    {tasks.map(task => (
+                        <div className='task' key={task.id}>
+                            <textarea defaultValue={task.task}></textarea>
+                            <div>
+                                <input type='checkbox' id={`check${task.id}`} defaultChecked={task.status === 'complete'} /> &nbsp;
+                                <label htmlFor={`check${task.id}`}>Done</label>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div  className='taskGroup'>
+                    <div className='Heading'>
+                        Daily Task
+                    </div>
+                    
+                        <div className='task' >
+                            <textarea ></textarea>
+                            <div>
+                                <input type='checkbox' /> &nbsp;
+                                <label >Done</label>
+                            </div>
+                        </div>
+                    
+                </div>
             </div>
-           
+
         </>
     );
 }
