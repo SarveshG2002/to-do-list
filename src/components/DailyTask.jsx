b1fee7a88dc79c9f306308088e0b982716fda22f
Sarvesh Gandhere <sarveshgandhere2002@gmail.com>
2024-06-21 23:37:45 +0530

    Daily TAsk Component Updated

--- src/components/DailyTask.jsx
+++ src/components/DailyTask.jsx
@@ -1,7 +1,151 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import { BASE_URL } from './Host.jsx';
 
 function Upcoming() {
-    return <div>Daily Task Page</div>;
+    const [text, setText] = useState("");
+    // const [updateText, setUpdateText] = useState("");
+    // const [updateId, setUpdateId] = useState("");
+    const [tasks, setTasks] = useState([]);
+    const [error, setError] = useState("");
+    const [updatedTasks, setUpdatedTasks] = useState({});
+
+    useEffect(() => {
+        // fetchTasks();
+    }, []);
+
+    const fetchTasks = async () => {
+        try {
+            const username = localStorage.getItem('username');
+            const response = await axios.post(`${BASE_URL}/api/getTodayTasks`, {
+                username: username
+            });
+            console.log(response.data)
+            if (response.data.success) {
+                setTasks(response.data.tasks);
+            } else {
+                setError(response.data.message);
+            }
+        } catch (error) {
+            console.error("Error fetching tasks:", error);
+            setError('An error occurred. Please try again.');
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            console.log(text)
+            const username = localStorage.getItem('username');
+            // if (!username) {
+            //     throw new Error('Username not found in localStorage');
+            // }
+            const response = await axios.post(`${BASE_URL}/api/addtodayTask`, {
+                username: username,
+                task: text
+            })
+            console.log(response.data)
+            if (response.data.success) {
+                setText("");
+                fetchTasks();
+
+            }
+        } catch (error) {
+            console.error("Error during login:", error);
+            setError('An error occurred. Please try again.');
+        }
+    }
+
+    const handleUpdateChange = (taskId, value) => {
+        setUpdatedTasks({
+            ...updatedTasks,
+            [taskId]: value
+        });
+    };
+
+    const handleUpdate = async (taskId) => {
+        try {
+            const updatedTaskText = updatedTasks[taskId];
+            const response = await axios.post(`${BASE_URL}/api/updateTask`, {
+                id: taskId,
+                task: updatedTaskText
+            });
+            if (response.data.success) {
+                fetchTasks(); // Refresh tasks after update
+                // Optionally clear updatedTasks state after successful update
+                setUpdatedTasks({}); // Clear updatedTasks state
+            } else {
+                setError(response.data.message);
+            }
+        } catch (error) {
+            console.error("Error updating task:", error);
+            setError('An error occurred. Please try again.');
+        }
+    };
+
+    const handleStatusChange = async (taskId, newStatus) => {
+        console.log("status change")
+        try {
+            const response = await axios.post(`${BASE_URL}/api/updateTaskStatus`, {
+                taskId: taskId,
+                newStatus: newStatus
+            });
+            console.log(response.data);
+            if (response.data.success) {
+                // Update tasks state after successful update
+                const updatedTasks = tasks.map(task => {
+                    if (task.id === taskId) {
+                        return { ...task, status: newStatus };
+                    }
+                    return task;
+                });
+                setTasks(updatedTasks);
+            } else {
+                setError(response.data.message);
+            }
+        } catch (error) {
+            console.error("Error updating task status:", error);
+            setError('An error occurred. Please try again.');
+        }
+    };
+
+    return (
+        <>
+            <form className='new-add' onSubmit={handleSubmit}>
+                <div className="form-group required">
+                    {/* <input type="text" className='form-control'/> */}
+                    <textarea value={text} onChange={(e) => setText(e.target.value)}></textarea>
+                </div>
+                <button className='btn btn-primary mt-3'>Add</button>
+            </form>
+
+            <div className='to-do'>
+                <div className='taskGroup'>
+                    
+                    {tasks.map(task => (
+                        <div className='task' key={task.id}>
+                            <textarea defaultValue={task.task} onChange={(e) => handleUpdateChange(task.id, e.target.value)}></textarea>
+                            <div>
+                                <div>
+                                    <input 
+                                        type='checkbox' 
+                                        id={`check${task.id}`} 
+                                        defaultChecked={task.status === 'complete'} 
+                                        onChange={(e) => handleStatusChange(task.id, e.target.checked ? 'complete' : 'incomplete')}
+                                        /> &nbsp;
+                                    <label htmlFor={`check${task.id}`}>Done</label>
+                                </div>
+                                
+                                <button className='btn btn-primary mt-1' onClick={(e) => handleUpdate(task.id)}>Update</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                
+            </div>
+
+        </>
+    );
 }
 
 export default Upcoming;
